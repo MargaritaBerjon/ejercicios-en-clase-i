6857d7c766611e61d3f86921a54cfecf90baa673
Miguel del Mazo <migueldelmazo@gmail.com>
2020-02-28 10:02:31 +0100

    Added m2.l1 example

---
+++ module-2/lesson-1/example/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// get numberAElement dom element
+const numberAElement = document.querySelector('.js-number-a');
+// get numberBElement dom element
+const numberBElement = document.querySelector('.js-number-b');
+// get resultElement dom element
+const resultElement = document.querySelector('.js-result');
+
+const add = function() {
+  // get numberAElement value
+  let numberAValue = numberAElement.value;
+  // parse string value to number value
+  numberAValue = parseInt(numberAValue);
+  // get numberBElement value
+  let numberBValue = numberBElement.value;
+  // parse string value to number value
+  numberBValue = parseInt(numberBValue);
+
+  // add values
+  const resultValue = numberAValue + numberBValue;
+  // check resultElement
+  if (isNaN(resultValue)) {
+    resultElement.value = 'Datos inválidos';
+  } else {
+    // set resultElement value in resultElement input element
+    resultElement.value = resultValue;
+  }
+};
+
+// listen changes in numberAElement input
+numberAElement.addEventListener('change', add);
+// listen changes in numberAElement input
+numberBElement.addEventListener('change', add);
